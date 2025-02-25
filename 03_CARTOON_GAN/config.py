7df822e5b5128809eb6bfecf8ed7c49653526f45
Chloe Toda <114105094+kloweee@users.noreply.github.com>
2025-02-25 13:13:15 -0500

    Create 03_CARTOON_GAN

---
+++ 03_CARTOON_GAN/config.py
@@ -0,0 +1,31 @@
+import torch 
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+TRAIN_DIR = "../02_CYCLE_GAN/data"
+TEST_DIR = "../02_CYCLE_GAN/data"
+VAL_DIR = "../02_CYCLE_GAN/data"
+BATCH_SIZE = 1
+LEARNING_RATE = 1e-5
+LAMBDA_IDENTITY = 0.0
+LAMBDA_CYCLE = 10
+NUM_WORKERS = 4
+NUM_EPOCHS = 200
+LOAD_MODEL = True
+SAVE_MODEL = True
+CHECKPOINT_GEN_R = "genr.pth.tar"
+CHECKPOINT_GEN_C = "genc.pth.tar"
+CHECKPOINT_CRITIC_R = "criticr.pth.tar"
+CHECKPOINT_CRITIC_C = "criticc.pth.tar"
+
+transforms = A.Compose(
+    [
+        A.Resize(width=256, height=256),
+        A.HorizontalFlip(p=0.5),
+        A.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5], max_pixel_value=255),
+        ToTensorV2(),
+    ],
+    is_check_shapes=False,
+    additional_targets={"image0":"image"},
+)
