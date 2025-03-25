8a68e881507cad018847c18a93889486dafa69b5
Chloe Toda <114105094+kloweee@users.noreply.github.com>
2025-03-25 14:01:10 -0400

    Delete 03_CARTOON_GAN directory

--- 03_CARTOON_GAN/00_check_if_installed.py
+++
@@ -1,8 +0,0 @@
-import importlib.util
-
-# For illustrative purposes.
-package_name = 'tqdm'
-
-spec = importlib.util.find_spec(package_name)
-if spec is None:
-    print(package_name +" is not installed")
--- 03_CARTOON_GAN/getpath.py
+++
@@ -1,7 +0,0 @@
-import sys
-PATH = sys.executable
-print(PATH)
-
-# /usr/bin/python
-# torch.version.cuda()
-# pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
